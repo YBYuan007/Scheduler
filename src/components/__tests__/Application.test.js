819a7fdd39da7c44605147d5e91241ada4d72e55
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2022-01-24 21:27:21 -0500

    add one test using fireEvent and waitForElement

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-import { render, cleanup, waitForElement } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -11,8 +16,15 @@ it.only("renders without crashing", () => {
 });
 
 it.only("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const {getByText} = render(<Application />);
+  const { getByText } = render(<Application />);
+  return waitForElement(() => getByText("Monday"));
+});
 
-  console.log("getbytext",Object.keys(getByText));
-  return waitForElement (() => getByText("Monday"));
+it.only("shows text 'Leopold Silvers' in the document after fire the click event on the 'Tuesday menu button ", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument()
+  });
 });
