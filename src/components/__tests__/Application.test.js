0cc0b9a6359bf965cdb870e79b255c626bdb3de2
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2022-01-24 21:43:06 -0500

    change promise to async/await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -11,20 +11,30 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it.only("renders without crashing", () => {
-  render(<Application />);
-});
+// it.only("renders without crashing", () => {
+//   render(<Application />);
+// });
 
 it.only("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
   return waitForElement(() => getByText("Monday"));
 });
 
-it.only("shows text 'Leopold Silvers' in the document after fire the click event on the 'Tuesday menu button ", () => {
+// it.only("shows text 'Leopold Silvers' in the document after fire the click event on the 'Tuesday menu button ", () => {
+//   const { getByText } = render(<Application />);
+
+//   return waitForElement(() => getByText("Monday")).then(() => {
+//     fireEvent.click(getByText("Tuesday"));
+//     expect(getByText("Leopold Silvers")).toBeInTheDocument()
+//   });
+// });
+
+it.only("shows text 'Leopold Silvers' in the document after fire the click event on the 'Tuesday menu button ", async() => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
+  await waitForElement(() => getByText("Monday")).then(() => {
     fireEvent.click(getByText("Tuesday"));
     expect(getByText("Leopold Silvers")).toBeInTheDocument()
   });
 });
+
